b2c26da9ec3e3628504abe84bddfae97325c9e77
Padraig McCarthy <padraig.mc1999@gmail.com>
2020-07-16 16:34:26 +0100

    custom ip address scanner added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-        ChatterServer.main(new String[]{"6066"});
-        ChatterClient.main(new String[]{"localhost", "6066"});
+        Scanner uInput = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Client? (y/n)");
+            String role = uInput.nextLine();
+
+            if (role == "y") {
+                System.out.println("Enter Server IP address: ");
+                String IPAddress = uInput.nextLine();
+
+                ChatterClient.main(new String[]{IPAddress, "6066"});
+            } else if (role == "n") {
+                ChatterServer.main(new String[]{"6066"});
+            }
+        }
+
+
+
     }
 }
