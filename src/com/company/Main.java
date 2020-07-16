661a698eea5eee09a3c779c7f7288c1a35b30555
Padraig McCarthy <padraig.mc1999@gmail.com>
2020-07-16 16:37:34 +0100

    bug fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,13 +13,15 @@ public class Main {
             System.out.println("Client? (y/n)");
             String role = uInput.nextLine();
 
-            if (role == "y") {
+            if (role.equals("y")) {
                 System.out.println("Enter Server IP address: ");
                 String IPAddress = uInput.nextLine();
 
                 ChatterClient.main(new String[]{IPAddress, "6066"});
-            } else if (role == "n") {
+                break;
+            } else if (role.equals("n")) {
                 ChatterServer.main(new String[]{"6066"});
+                break;
             }
         }
 
